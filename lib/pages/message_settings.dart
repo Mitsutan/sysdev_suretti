663e9ef1701ccb765b4bf3f5001939d7734ab939
asdfqwer0910 <2101385@s.asojuku.ac.jp>
2024-07-24 14:53:04 +0900

    メッセージ設定修正
    GitHub Actionで引っかかってたimport文を削除

--- lib/pages/message_settings.dart
+++ lib/pages/message_settings.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:sysdev_suretti/pages/message_post_confirmation.dart';
 
 class MyApp extends StatelessWidget {
