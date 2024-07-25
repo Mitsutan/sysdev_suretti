6f22a01ff3c19275a438133e058ef119c1a68142
Mitsutan <petitcom_hondou@icloud.com>
2024-07-25 11:28:49 +0900

    Update testhome.dart
    ユーザー情報の取得に失敗したらログイン画面に戻すエラーハンドリング
    フォーマットしたらなんかいろいろ変わった

--- lib/pages/testhome.dart
+++ lib/pages/testhome.dart
@@ -62,107 +62,116 @@ class Testhome extends ConsumerWidget {
     }
 
     if (!userData.isGotUserData) {
-      getUserData();
+      try {
+        getUserData();
+      } catch (e) {
+        log(e.toString());
+        Navigator.of(context, rootNavigator: true).pushAndRemoveUntil(
+            MaterialPageRoute(builder: (context) {
+          return const Loading();
+        }), (route) => false);
+      }
       return const Scaffold(
         body: Center(
           child: CircularProgressIndicator(),
         ),
       );
     } else {
-
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: const Text('Testhome'),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            // Text(user!.userMetadata!['username'].toString()),
-            Text(userData.nickname),
-            TextButton(
-                onPressed: () {
-                  Supabase.instance.client.auth.signOut();
-                  userData.updateIsGotUserData(false);
-                  Navigator.of(context, rootNavigator: true).pushAndRemoveUntil(
-                      MaterialPageRoute(builder: (context) {
-                    return const Loading();
-                  }), (route) => false);
-                },
-                child: const Text('Logout')),
-            // TextFormField(
-            //   decoration: const InputDecoration(labelText: 'Major'),
-            //   onChanged: (value) => beacon.major = int.parse(value),
-            //   keyboardType: TextInputType.number,
-            // ),
-            // TextFormField(
-            //   decoration: const InputDecoration(labelText: 'Minor'),
-            //   onChanged: (value) => beacon.minor = int.parse(value),
-            //   keyboardType: TextInputType.number,
-            // ),
-            ElevatedButton(
-                onPressed: () {
-                  sqlite.deleteDatabase(Supabase.instance.client.auth.currentUser!.id);
-                },
-                child: const Text('delete table')),
-            const Text('Scanned devices:'),
-            Expanded(
-              child: ListView.builder(
-                itemCount: beacon.scanResults.length,
-                itemBuilder: (context, index) {
-                  final result = beacon.scanResults[index];
-                  final major1 = result
-                      .advertisementData.manufacturerData.values.first
-                      .elementAt(18)
-                      .toRadixString(16);
-                  final major2 = result
-                      .advertisementData.manufacturerData.values.first
-                      .elementAt(19)
-                      .toRadixString(16);
-                  final minor1 = result
-                      .advertisementData.manufacturerData.values.first
-                      .elementAt(20)
-                      .toRadixString(16);
-                  final minor2 = result
-                      .advertisementData.manufacturerData.values.first
-                      .elementAt(21)
-                      .toRadixString(16);
-                  log('major: $major1$major2, minor: $minor1$minor2');
-                  sqlite.insertScanedUser(ScannedUser(
-                      userId:
-                          int.parse('$major1$major2$minor1$minor2', radix: 16),
-                      scannedAt: DateTime.now(),
-                      isGotPost: false));
-                  log(result.advertisementData.manufacturerData.values
-                      .toString());
-                  return ListTile(
-                    title: Text(result.device.remoteId.toString()),
-                    subtitle: Text(
-                        result.advertisementData.manufacturerData.toString()),
-                    trailing: Text('${result.rssi}'),
-                  );
-                },
-              ),
-            ),
-          ],
+      return Scaffold(
+        appBar: AppBar(
+          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+          title: const Text('Testhome'),
         ),
-      ),
-      floatingActionButton: beacon.isScanning()
-          ? FloatingActionButton(
-              onPressed: () {
-                beacon.stopBeacon();
-              },
-              backgroundColor: Colors.red,
-              child: const Icon(Icons.stop),
-            )
-          : FloatingActionButton(
-              onPressed: () {
-                beacon.startBeacon(beacon.major, beacon.minor);
-              },
-              child: const Text("SCAN"),
-            ),
-    );
-  }
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              // Text(user!.userMetadata!['username'].toString()),
+              Text(userData.nickname),
+              TextButton(
+                  onPressed: () {
+                    Supabase.instance.client.auth.signOut();
+                    userData.updateIsGotUserData(false);
+                    Navigator.of(context, rootNavigator: true)
+                        .pushAndRemoveUntil(
+                            MaterialPageRoute(builder: (context) {
+                      return const Loading();
+                    }), (route) => false);
+                  },
+                  child: const Text('Logout')),
+              // TextFormField(
+              //   decoration: const InputDecoration(labelText: 'Major'),
+              //   onChanged: (value) => beacon.major = int.parse(value),
+              //   keyboardType: TextInputType.number,
+              // ),
+              // TextFormField(
+              //   decoration: const InputDecoration(labelText: 'Minor'),
+              //   onChanged: (value) => beacon.minor = int.parse(value),
+              //   keyboardType: TextInputType.number,
+              // ),
+              ElevatedButton(
+                  onPressed: () {
+                    sqlite.deleteDatabase(
+                        Supabase.instance.client.auth.currentUser!.id);
+                  },
+                  child: const Text('delete table')),
+              const Text('Scanned devices:'),
+              Expanded(
+                child: ListView.builder(
+                  itemCount: beacon.scanResults.length,
+                  itemBuilder: (context, index) {
+                    final result = beacon.scanResults[index];
+                    final major1 = result
+                        .advertisementData.manufacturerData.values.first
+                        .elementAt(18)
+                        .toRadixString(16);
+                    final major2 = result
+                        .advertisementData.manufacturerData.values.first
+                        .elementAt(19)
+                        .toRadixString(16);
+                    final minor1 = result
+                        .advertisementData.manufacturerData.values.first
+                        .elementAt(20)
+                        .toRadixString(16);
+                    final minor2 = result
+                        .advertisementData.manufacturerData.values.first
+                        .elementAt(21)
+                        .toRadixString(16);
+                    log('major: $major1$major2, minor: $minor1$minor2');
+                    sqlite.insertScanedUser(ScannedUser(
+                        userId: int.parse('$major1$major2$minor1$minor2',
+                            radix: 16),
+                        scannedAt: DateTime.now(),
+                        isGotPost: false));
+                    log(result.advertisementData.manufacturerData.values
+                        .toString());
+                    return ListTile(
+                      title: Text(result.device.remoteId.toString()),
+                      subtitle: Text(
+                          result.advertisementData.manufacturerData.toString()),
+                      trailing: Text('${result.rssi}'),
+                    );
+                  },
+                ),
+              ),
+            ],
+          ),
+        ),
+        floatingActionButton: beacon.isScanning()
+            ? FloatingActionButton(
+                onPressed: () {
+                  beacon.stopBeacon();
+                },
+                backgroundColor: Colors.red,
+                child: const Icon(Icons.stop),
+              )
+            : FloatingActionButton(
+                onPressed: () {
+                  beacon.startBeacon(beacon.major, beacon.minor);
+                },
+                child: const Text("SCAN"),
+              ),
+      );
+    }
   }
 }
