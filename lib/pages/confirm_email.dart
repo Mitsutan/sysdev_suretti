adec6ed0e4b042a31c9b89112bbe5a75dd9e363c
Mitsutan <83488293+Mitsutan@users.noreply.github.com>
2024-08-04 17:13:32 +0900

    Update confirm_email.dart
    メール再送信機能実装
    自動ログインが機能しないこともあったので、手動でログイン画面に遷移できるボタンも追加

--- lib/pages/confirm_email.dart
+++ lib/pages/confirm_email.dart
@@ -1,13 +1,15 @@
 import 'dart:async';
+import 'dart:developer';
 import 'package:flutter/material.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 import 'package:sysdev_suretti/navigator.dart';
-import 'package:sysdev_suretti/pages/sign_up.dart';
+import 'package:sysdev_suretti/pages/login.dart';
 
 class ConfirmEmailPage extends StatefulWidget {
-  const ConfirmEmailPage({super.key});
+  const ConfirmEmailPage({super.key, required this.email});
 
   final String title = 'メール確認';
+  final String email;
 
   @override
   State<ConfirmEmailPage> createState() => _ConfirmEmailPageState();
@@ -16,9 +18,24 @@ class ConfirmEmailPage extends StatefulWidget {
 class _ConfirmEmailPageState extends State<ConfirmEmailPage> {
   Future<int>? totalPriceFuture;
 
+  void resendEmail() async {
+    try {
+      await Supabase.instance.client.auth
+          .resend(email: widget.email, type: OtpType.signup);
+    } catch (e) {
+      log("exception: resend", error: e);
+    }
+    SnackBar snackBar = const SnackBar(
+      content: Text("メールを再送信しました"),
+    );
+
+    if (mounted) {
+      ScaffoldMessenger.of(context).showSnackBar(snackBar);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-
     Supabase.instance.client.auth.onAuthStateChange.listen((data) {
       final AuthChangeEvent event = data.event;
       if (event == AuthChangeEvent.signedIn) {
@@ -41,15 +58,16 @@ class _ConfirmEmailPageState extends State<ConfirmEmailPage> {
           children: <Widget>[
             const Text(
                 "入力したメールアドレスに確認メールを送信しました。\nメール内のリンクをクリックして登録を完了してください。"),
+            ElevatedButton(
+                onPressed: resendEmail, child: const Text("メールを再送信する")),
             TextButton(
                 onPressed: () {
-                  Navigator.of(context, rootNavigator: true).pushAndRemoveUntil(
-                      MaterialPageRoute(builder: (context) {
-                    return const SignupPage();
-                  }), (route) => false);
+                  Navigator.of(context)
+                      .push(MaterialPageRoute(builder: (context) {
+                    return const LoginPage();
+                  }));
                 },
-                child: const Text("新規登録へ戻る")),
-            ElevatedButton(onPressed: () {}, child: const Text("メールを再送信する")),
+                child: const Text("ログインへ")),
           ],
         ),
       ),
