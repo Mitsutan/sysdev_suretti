c822875978a68033a1cd2320a1565dce9c89f75b
2101002ishikawa <2101002@s.asojuku.ac.jp>
2024-09-10 09:28:23 +0900

    定期更新

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,87 @@
+// import 'package:flutter_dotenv/flutter_dotenv.dart';
+// import 'dart:developer';
+// import 'package:flutter/material.dart';
+// import 'package:flutter_blue_plus/flutter_blue_plus.dart';
+// import 'package:flutter_riverpod/flutter_riverpod.dart';
+// import 'package:permission_handler/permission_handler.dart';
+// import 'package:supabase_flutter/supabase_flutter.dart';
+// import 'package:flutter_blue_plus/flutter_blue_plus.dart';
+// import 'package:sysdev_suretti/pages/loading.dart';
+
+// // エントリーポイント
+// Future<void> main() async {
+
+//   WidgetsFlutterBinding.ensureInitialized();
+
+//   // ログの設定
+//   void log(String level, String message) {
+//     debugPrint('$level: ${DateTime.now()}: $message');
+//   }
+
+//   try {
+//     // .env ファイルのロード
+//     await dotenv.load(fileName: ".env");
+//     log('INFO', '.env file loaded successfully');
+//   } catch (e) {
+//     log('SEVERE', 'Failed to load .env file: $e');
+//     return;
+//   }
+
+//   final supabaseUrl = dotenv.env['SUPABASE_URL'];
+//   final supabaseAnonKey = dotenv.env['SUPABASE_ANON_KEY'];
+
+//   if (supabaseUrl == null || supabaseAnonKey == null) {
+//     log('SEVERE', 'SUPABASE_URL or SUPABASE_ANON_KEY is not set in .env file');
+//     return;
+//   }
+
+//   await Supabase.initialize(
+//     url: supabaseUrl,
+//     anonKey: supabaseAnonKey,
+//   );
+//   // mitsutan
+//   // await Supabase.initialize(
+//   //   url: 'https://jeluoazapxqjksdfvftm.supabase.co', // ここにSupabaseプロジェクトのURLを入力
+//   //   anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImplbHVvYXphcHhxamtzZGZ2ZnRtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTkxOTI0MTAsImV4cCI6MjAzNDc2ODQxMH0.T2OLrQzZrv3hvs2khmLdlFb72XE-m7QOJdqNhVmIges', // ここにSupabaseプロジェクトのanonキーを入力
+//   // );
+
+//   // ログレベルの設定
+//   FlutterBluePlus.setLogLevel(LogLevel.verbose, color: true);
+
+
+//   runApp(const ProviderScope(child: MyApp()));
+// }
+
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
+
+//   Future<void> requestPermission() async {
+//     Map<Permission, PermissionStatus> statuses = await [
+//       Permission.bluetoothAdvertise,
+//       Permission.bluetoothScan,
+//       // Permission.bluetoothConnect,
+//       Permission.locationWhenInUse,
+//       Permission.locationAlways,
+//       // Permission.bluetooth
+//     ].request();
+//     log(statuses.toString(), name: 'PermissionStatus');
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+
+//     requestPermission();
+    
+//     return MaterialApp(
+//       title: 'Flutter Demo',
+//       theme: ThemeData(
+//         colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
+//         useMaterial3: true,
+//       ),
+//       home: const Loading(),
+//     );
+//   }
+// }
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'dart:developer';
 import 'package:flutter/material.dart';
@@ -5,8 +89,8 @@ import 'package:flutter_blue_plus/flutter_blue_plus.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:permission_handler/permission_handler.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
-import 'package:flutter_blue_plus/flutter_blue_plus.dart';
-import 'package:sysdev_suretti/pages/loading.dart';
+// import 'package:sysdev_suretti/pages/loading.dart';
+import 'package:sysdev_suretti/pages/testpage1.dart'; // これを追加
 
 // エントリーポイント
 Future<void> main() async {
@@ -78,7 +162,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
         useMaterial3: true,
       ),
-      home: const Loading(),
+      home: const TestPage1(), // これを変更
     );
   }
 }
--- lib/map.dart
+++ lib/map.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:permission_handler/permission_handler.dart';
+// import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class MapPage extends StatefulWidget {
   const MapPage({super.key});
