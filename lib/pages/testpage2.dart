a0cc267cb222476efb2d8add19235ae9bc20b014
Mitsutan <petitcom_hondou@icloud.com>
2024-09-11 10:28:41 +0900

    Update testpage2.dart
    remove unused package

--- lib/pages/testpage2.dart
+++ lib/pages/testpage2.dart
@@ -1,7 +1,5 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
-import 'package:flutter_hooks/flutter_hooks.dart';
-import 'package:sysdev_suretti/utils/enum_pages.dart';
 import 'package:sysdev_suretti/utils/page_notifier.dart';
 
 class TestPage2 extends StatefulWidget {
