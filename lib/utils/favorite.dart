63d32a3292ef10f25f3366128467a47b71cf4bc1
Mitsutan <petitcom_hondou@icloud.com>
2024-11-19 12:36:51 +0900

    Update favorite.dart
    ・同じメッセージを再受信した際に前回のいいね状態が反映されていなかったのを修正
    ・念のため例外処理を追加

--- lib/utils/favorite.dart
+++ lib/utils/favorite.dart
@@ -6,50 +6,56 @@ import 'package:supabase_flutter/supabase_flutter.dart';
 final supabase = Supabase.instance.client;
 
 /// データベースにいいねを登録する
-/// 
+///
 /// [messageId] メッセージID
-/// 
+///
 /// [userId] ユーザーID
 Future<void> likeMessage(int messageId, int userId) async {
-  await supabase
-      .from('favorites')
-      .insert({'message_id': messageId, 'user_id': userId});
-  // if (response.error != null) {
-  //   throw Exception('いいねに失敗しました: ${response.error!.message}');
-  // }
+  try {
+    await supabase
+        .from('favorites')
+        .insert({'message_id': messageId, 'user_id': userId});
+  } catch (e) {
+    log("faild to like message $messageId - $userId",
+        error: e, name: "FAVORITE");
+  }
 }
 
 /// データベースからいいねを取り消す
-/// 
+///
 /// [messageId] メッセージID
-/// 
+///
 /// [userId] ユーザーID
 Future<void> unlikeMessage(int messageId, int userId) async {
-  await supabase
-      .from('favorites')
-      .delete()
-      .match({'message_id': messageId, 'user_id': userId});
-  // if (response.error != null) {
-  //   throw Exception('いいねの取り消しに失敗しました: ${response.error!.message}');
-  // }
+  try {
+    await supabase
+        .from('favorites')
+        .delete()
+        .match({'message_id': messageId, 'user_id': userId});
+  } catch (e) {
+    log("faild to unlike message $messageId - $userId",
+        error: e, name: "FAVORITE");
+  }
 }
 
 /// メッセージがいいねされているか確認する
-/// 
+///
 /// [messageId] メッセージID
-/// 
+///
 /// [userId] ユーザーID
 Future<bool> isMessageLiked(int messageId, int userId) async {
-  final response = await supabase
-      .from('favorites')
-      .select()
-      .match({'message_id': messageId, 'user_id': userId});
-  // if (response.error != null) {
-  //   throw Exception('いいね状態の確認に失敗しました: ${response.error!.message}');
-  // }
-  // return response.data.isNotEmpty;
+  try {
+    final response = await supabase
+        .from('favorites')
+        .select()
+        .match({'message_id': messageId, 'user_id': userId});
 
-  return response.isNotEmpty;
+    return response.isNotEmpty;
+  } catch (e) {
+    log("faild to check message $messageId - $userId",
+        error: e, name: "FAVORITE");
+    return false;
+  }
 }
 
 // Future<bool> isMessageLikedRealtime(int messageId, int userId) async {
@@ -80,30 +86,44 @@ Future<bool> isMessageLiked(int messageId, int userId) async {
 // }
 
 /// メッセージがいいねされているかリアルタイムで確認する
-/// 
+///
 /// [messageId] メッセージID
-/// 
+///
 /// [userId] ユーザーID
 Stream<bool> isMessageLikedRealtime(int messageId, int userId) {
   final StreamController<bool> controller = StreamController<bool>();
+  bool isFetched = false;
 
-  supabase.channel('favorites').onPostgresChanges(
-    event: PostgresChangeEvent.all,
-    schema: 'public',
-    table: 'favorites',
-    filter: PostgresChangeFilter(
-      type: PostgresChangeFilterType.eq, column: "user_id", value: userId),
-    callback: (data) {
-      log('update favData: $data');
+  if (!isFetched) {
+    isMessageLiked(messageId, userId).then((isLiked) {
+      log('isLiked: $isLiked');
+      controller.add(isLiked);
+      isFetched = true;
+    });
+  }
 
-      if (data.newRecord['message_id'] == messageId) {
-        log('いいね状態が変更されました: ${data.newRecord}');
-        controller.add(true);
-      } else {
-        controller.add(false);
-      }
-    }
-  ).subscribe();
+  supabase
+      .channel('favorites')
+      .onPostgresChanges(
+          event: PostgresChangeEvent.all,
+          schema: 'public',
+          table: 'favorites',
+          filter: PostgresChangeFilter(
+              type: PostgresChangeFilterType.eq,
+              column: "user_id",
+              value: userId),
+          callback: (data) {
+            log('update favData: $data');
+
+            if (data.newRecord['message_id'] == messageId) {
+              log('いいねされました: ${data.newRecord}');
+              controller.add(true);
+            } else {
+              log('いいねが取り消されました: ${data.oldRecord}');
+              controller.add(false);
+            }
+          })
+      .subscribe();
 
   return controller.stream;
 }
