050b0575b538ca4f6d5c5d5fd17b9fa8c48c22c6
Mitsutan <petitcom_hondou@icloud.com>
2024-12-17 12:56:26 +0900

    providerを用いてDB読込
    ”It looks like you've created the database class AppDatabase multiple times. When these two databases use the same QueryExecutor, race conditions will occur and might corrupt the database. ”とか言われるけど大きな問題ではない（ほんと？）

---
+++ lib/utils/db_provider.dart
@@ -0,0 +1,12 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:sysdev_suretti/models/bookmarks.dart';
+
+
+
+final dbProvider = Provider((ref) => DBProvider());
+class DBProvider {
+
+  static final AppDatabase _appDatabase = AppDatabase();
+  AppDatabase get database => _appDatabase;
+  
+}
