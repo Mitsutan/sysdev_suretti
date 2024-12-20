3b4a04251c18bd03783267dfda56873e1bfb3889
Mitsutan <83488293+Mitsutan@users.noreply.github.com>
2024-12-20 20:29:18 +0900

    Update provider.dart
    一つ前のコミットだと新規ユーザーがクラッシュするのを修正

--- lib/utils/provider.dart
+++ lib/utils/provider.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
 final userDataProvider = ChangeNotifierProvider((ref) => UserData());
@@ -9,8 +10,15 @@ class UserData extends ChangeNotifier {
 
   dynamic get userData => _userData;
 
+  late final SharedPreferences _prefs;
+
   UserData() {
+    _initPrefs();
     watchUserdata();
+  }
+
+  Future<void> _initPrefs() async {
+    _prefs = await SharedPreferences.getInstance();
   }
 
   void watchUserdata() {
@@ -21,6 +29,16 @@ class UserData extends ChangeNotifier {
         .stream(primaryKey: ['auth_id'])
         .eq('auth_id', supabase.auth.currentUser!.id)
         .listen((data) {
+          if (_prefs.getInt('major') == null ||
+              _prefs.getInt('minor') == null) {
+            String userId = int.parse(data.first['user_id'].toString())
+                .toRadixString(16)
+                .padLeft(8, '0');
+            _prefs.setInt(
+                'major', int.parse(userId.substring(0, 4), radix: 16));
+            _prefs.setInt(
+                'minor', int.parse(userId.substring(4, 8), radix: 16));
+          }
           updateUserData(data.first);
         });
   }
