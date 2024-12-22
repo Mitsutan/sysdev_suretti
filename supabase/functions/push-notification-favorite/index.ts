aff1e164d52e99756509663f1d64225a33b10347
Mitsutan <83488293+Mitsutan@users.noreply.github.com>
2024-12-22 21:49:04 +0900

    Create index.ts
    favoritesテーブルに行が追加されたら、対象レコードのmessage_idに紐づくユーザーへプッシュ通知を送信

---
+++ supabase/functions/push-notification-favorite/index.ts
@@ -0,0 +1,100 @@
+import { createClient } from 'npm:@supabase/supabase-js@2'
+import { JWT } from 'npm:google-auth-library@9'
+import serviceAccount from '../service-account.json' with { type: 'json' }
+
+interface Favorites {
+  message_id: string
+  user_id: string
+}
+
+interface WebhookPayload {
+  type: 'INSERT'
+  table: string
+  record: Favorites
+  schema: 'public'
+}
+
+const supabase = createClient(
+  Deno.env.get('SUPABASE_URL')!,
+  Deno.env.get('SUPABASE_SERVICE_ROLE_KEY')!
+)
+
+Deno.serve(async (req) => {
+  const payload: WebhookPayload = await req.json()
+  console.log(payload)  
+
+  const { data: msgData } = await supabase
+    .from('messages')
+    .select('*')
+    .eq('message_id', payload.record.message_id)
+    .single()
+  console.log(msgData)
+    
+  const { data: userData } = await supabase
+    .from('users')
+    .select('fcm_token')
+    .eq('user_id', msgData.user_id)
+    .single()
+  console.log(userData)
+  
+  
+
+  const fcmToken = userData!.fcm_token as string
+
+  const accessToken = await getAccessToken({
+    clientEmail: serviceAccount.client_email,
+    privateKey: serviceAccount.private_key,
+  })
+
+  const res = await fetch(
+    `https://fcm.googleapis.com/v1/projects/${serviceAccount.project_id}/messages:send`,
+    {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${accessToken}`,
+      },
+      body: JSON.stringify({
+        message: {
+          token: fcmToken,
+          notification: {
+            title: `あなたの投稿がいいねされました`,
+            body: msgData.message_text,
+          },
+        },
+      }),
+    }
+  )
+
+  const resData = await res.json()
+  if (res.status < 200 || 299 < res.status) {
+    throw resData
+  }
+
+  return new Response(JSON.stringify(resData), {
+    headers: { 'Content-Type': 'application/json' },
+  })
+})
+
+const getAccessToken = ({
+  clientEmail,
+  privateKey,
+}: {
+  clientEmail: string
+  privateKey: string
+}): Promise<string> => {
+  return new Promise((resolve, reject) => {
+    const jwtClient = new JWT({
+      email: clientEmail,
+      key: privateKey,
+      scopes: ['https://www.googleapis.com/auth/firebase.messaging'],
+    })
+    jwtClient.authorize((err, tokens) => {
+      if (err) {
+        reject(err)
+        return
+      }
+      resolve(tokens!.access_token!)
+    })
+  })
+}
