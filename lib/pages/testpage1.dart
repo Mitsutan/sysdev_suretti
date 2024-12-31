b86ef25247265d63dc4521eb29c5218600c5da60
Mitsutan <83488293+Mitsutan@users.noreply.github.com>
2024-12-31 17:21:52 +0900

    Update testpage1.dart
    ローカル通知のテストボタン追加

--- lib/pages/testpage1.dart
+++ lib/pages/testpage1.dart
@@ -1,10 +1,10 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:sysdev_suretti/pages/login.dart';
 import 'package:sysdev_suretti/pages/map.dart';
 import 'testpage3.dart';
 import 'package:sysdev_suretti/pages/sign_up.dart';
-
 
 class TestPage1 extends StatefulWidget {
   const TestPage1({super.key});
@@ -76,10 +76,29 @@ class _TestPage1State extends State<TestPage1> {
                 onPressed: () {
                   Navigator.of(context)
                       .push(MaterialPageRoute(builder: (context) {
-                    return const MapPage(location: {}, center: null,); // MapPageに遷移
+                    return const MapPage(
+                      location: {},
+                      center: null,
+                    ); // MapPageに遷移
                   }));
                 },
                 child: const Text('Go to Map')), // 新しいボタンを追加
+            ElevatedButton(
+                onPressed: () async {
+                  const AndroidNotificationDetails androidNotificationDetails =
+                      AndroidNotificationDetails(
+                          'your channel id', 'your channel name',
+                          channelDescription: 'your channel description',
+                          importance: Importance.max,
+                          priority: Priority.high,
+                          ticker: 'ticker');
+                  const NotificationDetails notificationDetails =
+                      NotificationDetails(android: androidNotificationDetails);
+                  await FlutterLocalNotificationsPlugin().show(
+                      0, 'plain title', 'plain body', notificationDetails,
+                      payload: 'item x');
+                },
+                child: const Text('通知')), // プッシュ通知
           ],
         ),
       ),
